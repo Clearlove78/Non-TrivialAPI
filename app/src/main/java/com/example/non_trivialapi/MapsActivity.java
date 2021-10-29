3554cedb9632abcdb02b5a290332a0c9c19df1ed
Xia junwe <jxia27@binghamton.edu>
2021-10-29 03:25:47 -0400

    Forth commit: add mark in map and show the current location with latitude and longitude

--- app/src/main/java/com/example/non_trivialapi/MapsActivity.java
+++ app/src/main/java/com/example/non_trivialapi/MapsActivity.java
@@ -1,317 +1,179 @@
-/*
- * Copyright (c) 2016 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+package com.example.non_trivialapi;
 
-package com.example.non_trivialapi.;
-
-import android.content.Intent;
-import android.content.IntentSender;
+import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.LocationAvailability;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        GoogleMap.OnMarkerClickListener, LocationListener {
 
-    private GoogleMap mMap;
-    private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
-    private LocationRequest mLocationRequest;
-    private boolean mLocationUpdateState;
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private static final int REQUEST_CHECK_SETTINGS = 2;
-    private static final int PLACE_PICKER_REQUEST = 3;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+private GoogleMap mMap;
+private Double latitude, longitude;
+        ArrayList<LatLng> markerPoints= new ArrayList<>();
+
+
+        Button btnShowLocation;
+private static final int REQUEST_CODE_PERMISSION = 2;
+        String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+
+        // GPSTracker class
+        com.example.non_trivialapi.GPSTracker gps;
+
+@Override
+public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        try {
+        if (ActivityCompat.checkSelfPermission(this, mPermission)
+        != getPackageManager().PERMISSION_GRANTED) {
+
+        ActivityCompat.requestPermissions(this, new String[]{mPermission},
+        REQUEST_CODE_PERMISSION);
+
+        // If any permission above not allowed by user, this condition will
+        //  execute every time, else your else part will work
+        }
+        } catch (Exception e) {
+        e.printStackTrace();
+        }
+
+        btnShowLocation = (Button) findViewById(R.id.button);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+
+
+@Override
+public void onClick(View arg0) {
+        // create class object
+        gps = new com.example.non_trivialapi.GPSTracker(MapsActivity.this);
+
+        // check if GPS enabled
+        if (gps.canGetLocation()) {
+
+        latitude = gps.getLatitude();
+        longitude = gps.getLongitude();
+
+        Toast.makeText(getApplicationContext(), "Location:\nLatitude: "
+        + latitude + "\nLongitude: " + longitude, Toast.LENGTH_LONG).show();
+        ShowMap();
+        } else {
+        // can't get location
+        // GPS or Network is not enabled
+        // Ask user to enable GPS/network in settings
+        gps.showSettingsAlert();
+        }
+        }
+        });
+
+
+        }
+        public void onMapSearch(View view) {
+                EditText locationSearch = (EditText) findViewById(R.id.editText);
+                String location = locationSearch.getText().toString();
+                List<Address> addressList = null;
+
+                if (location != null || !location.equals("")) {
+                        Geocoder geocoder = new Geocoder(this);
+                        try {
+                                addressList = geocoder.getFromLocationName(location, 1);
+
+                        } catch (IOException e) {
+                                e.printStackTrace();
+                        }
+                        Address address = addressList.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                        mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+                        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                }
+        }
+public void ShowMap() {
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
         }
 
-        createLocationRequest();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadPlacePicker();
-            }
-        });
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
+/**
+ * Manipulates the map once available.
+ * This callback is triggered when the map is ready to be used.
+ * This is where we can add markers or lines, add listeners or move the camera.
+ * If Google Play services is not installed on the device, the user will be prompted to install
+ * it inside the SupportMapFragment. This method will only be triggered once the user has
+ * installed Google Play services and returned to the app.
+ */
+@Override
+public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng myPlace = new LatLng(40.73, -73.99);  // this is New York
-        mMap.addMarker(new MarkerOptions().position(myPlace).title("My Favorite City"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace, 12));
-
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.setOnMarkerClickListener(this);
-    }
-
-    private void setUpMap() {
-        if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-            return;
-        }
-
-        mMap.setMyLocationEnabled(true); // 1
-
-        LocationAvailability locationAvailability =
-                LocationServices.FusedLocationApi.getLocationAvailability(mGoogleApiClient); // 2
-        if (null != locationAvailability && locationAvailability.isLocationAvailable()) {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient); // 3
-
-            if (mLastLocation != null) {    // 4
-                LatLng currentLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation
-                        .getLongitude());
-                placeMarkerOnMap(currentLocation);
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 12));
-            }
-        }
-    }
-
-    protected void placeMarkerOnMap(LatLng location) {
-        MarkerOptions markerOptions = new MarkerOptions().position(location); //1
-        String titleStr = getAddress(location);
-        markerOptions.title(titleStr);
-
-        mMap.addMarker(markerOptions); //2
-    }
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng latLng) {
 
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        setUpMap();
-        if (mLocationUpdateState) {
-            startLocationUpdates();
-        }
-    }
+                        // Adding new item to the ArrayList
+                        markerPoints.add(latLng);
 
-    @Override
-    public void onConnectionSuspended(int i) {
+                        // Creating MarkerOptions
+                        MarkerOptions options = new MarkerOptions();
 
-    }
+                        // Setting the position of the marker
+                        options.position(latLng);
 
-    @Override
-    public void onLocationChanged(Location location) {
-        mLastLocation = location;
-        if (null != mLastLocation) {
-            placeMarkerOnMap(new LatLng(mLastLocation.getLatitude(), mLastLocation
-                    .getLongitude()));
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        return false;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if( mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
-            mGoogleApiClient.disconnect();
-        }
-    }
-
-    private String getAddress( LatLng latLng ) {
-        Geocoder geocoder = new Geocoder( this ); // 1
-        String addressText = "";
-        List<Address> addresses = null;
-        Address address = null;
-        try {
-            addresses = geocoder.getFromLocation( latLng.latitude, latLng.longitude, 1 ); // 2
-            if (null != addresses && !addresses.isEmpty()) { // 3
-                address = addresses.get(0);
-                for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
-                    addressText += (i == 0)?address.getAddressLine(i):("\n" + address.getAddressLine(i));
-                }
-            }
-        } catch (IOException e ) {
-        }
-        return addressText;
-    }
-
-
-    protected void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-            return;
-        }
-        LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient, mLocationRequest, this);
-    }
-
-    protected void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(mLocationRequest);
-
-        PendingResult<LocationSettingsResult> result =
-                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
-                        builder.build());
-
-        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
-            @Override
-            public void onResult(@NonNull LocationSettingsResult result) {
-                final Status status = result.getStatus();
-                switch (status.getStatusCode()) {
-                    case LocationSettingsStatusCodes.SUCCESS:
-                        mLocationUpdateState = true;
-                        startLocationUpdates();
-                        break;
-                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                        try {
-                            status.startResolutionForResult(MapsActivity.this, REQUEST_CHECK_SETTINGS);
-                        } catch (IntentSender.SendIntentException e) {
+                        if (markerPoints.size() == 1)
+                        {
+                                options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+                                options.title("Point");
                         }
-                        break;
-                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                        break;
+                        // Add new marker to the Google Map Android API V2
+                        mMap.addMarker(options);
                 }
-            }
         });
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CHECK_SETTINGS) {
-            if (resultCode == RESULT_OK) {
-                mLocationUpdateState = true;
-                startLocationUpdates();
-            }
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+
+        // Add a marker in current location and move the camera
+        LatLng mylocation = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(mylocation).title("current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mylocation, 15));
+        Circle circle = mMap.addCircle(new CircleOptions()
+        .center(new LatLng(latitude, longitude))
+        .radius(600)
+        .strokeColor(Color.RED)
+        .fillColor(Color.TRANSPARENT));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+        }
+        mMap.setMyLocationEnabled(true);
+
         }
 
-        if (requestCode == PLACE_PICKER_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                Place place = PlacePicker.getPlace(this, data);
-                String addressText = place.getName().toString();
-                addressText += "\n" + place.getAddress().toString();
-
-                placeMarkerOnMap(place.getLatLng());
-            }
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        LocationServices.FusedLocationApi.removeLocationUpdates(
-                mGoogleApiClient, this);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mGoogleApiClient.isConnected() && !mLocationUpdateState) {
-            startLocationUpdates();
-        }
-    }
-
-    private void loadPlacePicker() {
-        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder(); // 25
-
-        try {
-            startActivityForResult(builder.build(MapsActivity.this), PLACE_PICKER_REQUEST);
-        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
-        }
-    }
 }
